f24fc286185eae77e6ca10ac5e94578761bea1a6
xuezeao <dfzx.123@163.com>
2016-08-10 09:41:34 +0800

    “time : :q
    git commit -m “time

--- Qt_Dev/untitled1/mainwindow.cpp
+++ Qt_Dev/untitled1/mainwindow.cpp
@@ -1,14 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QLineEdit>
+#include <QDateTime>
+#include <QLabel>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->lineEdit_password->setEchoMode(QLineEdit::Password);
-  //  ui->lineEdit_account
-    
+
+    QDateTime time = QDateTime::currentDateTime();
+    QString str = time.toString("yyyy-MM-dd hh:mm:ss ddd");
+    ui->label_time->setText(str);
 
 }
 
